4c906af6fe18bfaf52f7fda72bb91092f13fbfd5
Sunny Ripert <sunny@sunfox.org>
2016-07-03 15:31:21 +0200

    Remove extraneaous self

--- lib/devise/models/track_locale.rb
+++ lib/devise/models/track_locale.rb
@@ -2,8 +2,8 @@ module Devise
   module Models
     module TrackLocale
       def set_locale!
-        if self.locale.to_s != I18n.locale.to_s
-          self.update_column(:locale, I18n.locale.to_s)
+        if locale.to_s != I18n.locale.to_s
+          update_column(:locale, I18n.locale.to_s)
         end
       end
     end
